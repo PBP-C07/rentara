92d4e0cfc6f6078903cc7d37988746edf7fa83a2
meisyanajla <meisyaanajlaa@gmail.com>
2024-10-24 11:37:39 +0700

    Adding Register and Login

--- main/views.py
+++ main/views.py
@@ -1,4 +1,49 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.urls import reverse
 
+def show_main(request):
+    context = {
+        'apps' : 'Rentara+',
+    }
+
+    return render(request, "main.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            if not request.POST.get('remember_me'):
+                request.session.set_expiry(0)
+
+            else:
+                request.session.set_expiry(1209600)
+                
+            if user.is_staff:
+                return HttpResponseRedirect(reverse('joinpartner:manage_partners'))
+            
+            return redirect('main:show_main')
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
