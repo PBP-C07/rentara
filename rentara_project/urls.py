2b3424fe98149bb9f6d3e3c6406314b8d1bd488d
agustinisridewi <agus.tini31@ui.ac.id>
2024-10-25 21:31:28 +0700

    add navbar rentdriver

--- rentara_project/urls.py
+++ rentara_project/urls.py
@@ -20,10 +20,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rentdriver.urls')),
-    path('', include('bookmark.urls')),
-    path('', include('joinpartner.urls')),
-    path('', include('report.urls')),
-    path('', include('reviews.urls')),
     path('', include('main.urls')),
+    path('', include('bookmark.urls')),
+    # path('', include('joinpartner.urls')),
+    # path('', include('report.urls')),
+    # path('', include('reviews.urls')),
     path('', include('sewajual.urls')),
 ] 
