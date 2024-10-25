25424e6647533d469cf82a1517cabef3b4bc2292
freiazulaika <freiazulaika27@gmail.com>
2024-10-25 16:29:20 +0700

    add sewajual, main, navbar

--- sewajual/urls.py
+++ sewajual/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import vehicle_list, full_info, AdminVehicleDeleteView, AdminVehicleUpdateView, AdminVehicleListView, AdminVehicleCreateView
+from .views import vehicle_list, full_info, admin_vehicle_list, add_vehicle, edit_vehicle, delete_vehicle
+
+app_name = 'sewajual'
 
 urlpatterns = [
     path('katalog/', vehicle_list, name='vehicle_list'),
     path('katalog/<uuid:pk>/', full_info, name='full_info'),
-    path('admin/vehicles/', AdminVehicleListView.as_view(), name='admin-vehicles'),
-    path('admin/vehicles/add/', AdminVehicleCreateView.as_view(), name='admin-vehicle-add'),
-    path('admin/vehicles/<uuid:pk>/edit/', AdminVehicleUpdateView.as_view(), name='admin-vehicle-edit'),
-    path('admin/vehicles/<uuid:pk>/delete/', AdminVehicleDeleteView.as_view(), name='admin-vehicle-delete'),
+    path('vehicles/adm/', admin_vehicle_list, name='admin_vehicle_list'),
+    path('vehicles/adm/add/', add_vehicle, name='add_vehicle'),
+    path('vehicles/adm/<uuid:pk>/edit/', edit_vehicle, name='edit_vehicle'),
+    path('vehicles/adm/<uuid:pk>/delete/', delete_vehicle, name='delete_vehicle'),
 ]
