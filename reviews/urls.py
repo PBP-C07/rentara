2fe5d937ea8b1ec17270c08b5ede443119afadca
meisyanajla <meisyaanajlaa@gmail.com>
2024-10-25 20:42:33 +0700

    Adding Create Reviews

---
+++ reviews/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from reviews.views import show_reviews, create_reviews 
+
+app_name = 'reviews'
+
+urlpatterns = [
+    path('show-reviews/', show_reviews, name='show_reviews'),
+    path('create-reviews', create_reviews, name='create_reviews'),
+]
