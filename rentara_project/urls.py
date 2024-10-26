81558f14220092632a8791dc15881022955fcb10
meisyanajla <meisyaanajlaa@gmail.com>
2024-10-27 03:21:05 +0700

    Revising NavBar and Adding Landing Page

--- rentara_project/urls.py
+++ rentara_project/urls.py
@@ -19,11 +19,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
     path('', include('rentdriver.urls')),
     path('', include('bookmark.urls')),
     path('', include('joinpartner.urls')),
-    path('', include('report.urls')),
+    # path('', include('report.urls')),
     path('reviews/', include('reviews.urls')),
-    path('', include('main.urls')),
-    # path('', include('sewajual.urls')),
+    path('', include('sewajual.urls')),
 ] 
