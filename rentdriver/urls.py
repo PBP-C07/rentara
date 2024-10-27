a7aae3f52492ca883598c0fa1d229331ded5ad9d
agustinisridewi <agus.tini31@ui.ac.id>
2024-10-28 03:55:27 +0700

    last commit rentdriver

--- rentdriver/urls.py
+++ rentdriver/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from rentdriver.views import show_drivers
+from rentdriver.views import show_main, edit_driver
 
 app_name = 'rentdriver'
 
 urlpatterns = [
-    path('', show_drivers, name='show_drivers'),
+    path('edit_driver/<uuid:driver_id>/', edit_driver, name='edit_driver'),
+    path('show-main/', show_main, name='show_main'),
 ]
