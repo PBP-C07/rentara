47c8420a7cd7b10b14298b9f1821788aca7c3d40
felfelita <felitadiyanti@gmail.com>
2024-10-27 19:56:01 +0700

    Menjelaskan perubahan lokal

--- report/views.py
+++ report/views.py
@@ -1,7 +1,10 @@
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required 
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from report.forms import ReportForm  
 from report.models import Report, Vehicle
 from django.utils import timezone
@@ -10,9 +13,7 @@ from django.core import serializers
 from django.utils.html import strip_tags
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from django.http import HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-import datetime
+from django.contrib.auth.forms import AuthenticationForm
 
 
 @login_required(login_url='/login')  # Redirect to login page if not logged in
@@ -27,7 +28,27 @@ def create_report_entry(request):
             report_entry.user = request.user
             report_entry.save()
             messages.success(request, 'Laporan berhasil dibuat!')
-            return redirect('main:show_main')  # Pastikan URL ini benar
+            return redirect('main:show_main') 
+    
+    context = {
+        'form': form,
+        'vehicles': vehicles,
+        'order_date': order_date,
+    }
+    return render(request, "main_report.html", context)
+
+def add_report(request):
+    vehicles = Vehicle.objects.filter(availability=True)
+    form = ReportForm(request.POST or None)
+    order_date = timezone.now().date()  # Assuming the order date is today
+
+    if request.method == "POST":
+        if form.is_valid():
+            report_entry = form.save(commit=False)
+            report_entry.user = request.user
+            report_entry.save()
+            messages.success(request, 'Laporan berhasil dibuat!')
+            return redirect('report:create_report_entry') 
     
     context = {
         'form': form,
@@ -41,7 +62,9 @@ def show_reports(request):
     report_entries = Report.objects.all()  
 
     context = {
-        'reports': report_entries  
+        'reports': report_entries,  
+        'empty_message': "No reports found.",  
+        'random_image': 'static/image/logo.png'  # Specify path to a random image
     }
 
     return render(request, "report_list.html", context)
@@ -64,6 +87,23 @@ def login_user(request):
 
    context = {'form': form}
    return render(request, 'login.html', context)
+
+def edit_report(request, id):
+    laporan = Report.objects.get(pk = id)
+
+    form = ReportForm(request.POST or None, instance=laporan)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('report:create_report_entry'))
+
+    context = {'form': form}
+    return render(request, "edit_report.html", context)
+
+def delete_report(request, id):
+    laporan = Report.objects.get(pk = id)
+    laporan.delete()
+    return HttpResponseRedirect(reverse('report:create_report_entry'))
 
 def show_xml(request):
     data = Report.objects.filter(user=request.user)
@@ -89,11 +129,11 @@ def report_entry_ajax(request):
     issue_type = strip_tags(request.POST.get("issue_type"))
     user = request.user
 
-    new_thrift = Report(
+    new_report = Report(
         vehicle=vehicle, description=description, 
         issue_type=issue_type,
         user=user 
     )
-    new_thrift.save()
+    new_report.save()
 
     return HttpResponse(b"CREATED", status=201)
