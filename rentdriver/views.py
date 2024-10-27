4d9f06ec49b7d88d3782c787f40893bbfe093781
agustinisridewi <agus.tini31@ui.ac.id>
2024-10-28 03:55:27 +0700

    last commit rentdriver

--- rentdriver/views.py
+++ rentdriver/views.py
@@ -1,7 +1,9 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from rentdriver.forms import DriverForm
 from rentdriver.models import Driver
+from django.http import JsonResponse
 import json
 
 def load_drivers_from_json(file_path = 'rentdriver/data/drivers.json'):
@@ -19,7 +21,7 @@ def load_drivers_from_json(file_path = 'rentdriver/data/drivers.json'):
             )
 
 @login_required(login_url='main:login')
-def show_drivers(request):
+def show_main(request):
     load_drivers_from_json()
 
     drivers = Driver.objects.all()
@@ -30,5 +32,31 @@ def show_drivers(request):
 
     return render(request, "rentdriver.html", context)
 
+from django.http import JsonResponse
 
 
+@staff_member_required
+@login_required(login_url='main:login')
+def edit_driver(request, driver_id):
+    driver = get_object_or_404(Driver, id=driver_id)
+    if request.method == 'POST':
+        form = DriverForm(request.POST, instance=driver)
+        if form.is_valid():
+            form.save()
+            if request.is_ajax():
+                # Send a JSON response if the request is AJAX
+                return JsonResponse({
+                    'success': True,
+                    'name': driver.name,
+                    'phone_number': driver.phone_number,
+                    'vehicle_type': driver.vehicle_type,
+                    'experience_years': driver.experience_years,
+                })
+            return redirect('show_main')
+    else:
+        form = DriverForm(instance=driver)
+
+    if request.is_ajax():
+        # Render the form as HTML for an AJAX GET request
+        return render(request, 'partials/edit_driver_form.html', {'form': form, 'driver': driver})
+    return render(request, 'edit_driver.html', {'form': form, 'driver': driver})
