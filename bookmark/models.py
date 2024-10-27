5be13c1d18d0aa127d01c04b29e2d231a942275c
agustinisridewi <agus.tini31@ui.ac.id>
2024-10-27 20:40:02 +0700

    commit

--- bookmark/models.py
+++ bookmark/models.py
@@ -7,3 +7,5 @@ class Bookmark(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='bookmarks')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # Reference to the user who bookmarked
+
+    
