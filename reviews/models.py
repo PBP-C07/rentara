466e11ff34a3a4b47e457e412676fd7973db3c98
meisyanajla <meisyaanajlaa@gmail.com>
2024-10-27 20:56:54 +0700

    Make Review Card

--- reviews/models.py
+++ reviews/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 import uuid
 
 class Reviews(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=25)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     time = models.DateTimeField(auto_now_add=True)
-    rating = models.PositiveIntegerField()
-    description = models.TextField()
-    helpful_votes = models.PositiveIntegerField(default=0)
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.TextField(max_length=195)
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from reviews.views import show_reviews, create_reviews 
+from reviews.views import show_reviews, create_reviews, edit_review, delete_review
 
 app_name = 'reviews'
 
 urlpatterns = [
     path('show-reviews/', show_reviews, name='show_reviews'),
     path('create-reviews/', create_reviews, name='create_reviews'),
+    path('edit-review/<uuid:id>', edit_review, name='edit_review'),
+    path('delete/<uuid:id>', delete_review, name='delete_review'),
 ]
