efc9f44e9211e7707d9a6c15dc779ef7025b913e
agustinisridewi <agus.tini31@ui.ac.id>
2024-10-27 17:10:10 +0700

    merge with update

--- bookmark/views.py
+++ bookmark/views.py
@@ -6,37 +6,17 @@ from sewajual.models import Vehicle
 from .models import Bookmark
 
 @login_required
-def show_main(request):
-    # Fetch all bookmarked vehicles for the logged-in user
+def bookmarked_vehicles(request):
     bookmarks = Bookmark.objects.filter(user=request.user).select_related('vehicle')
-    
-    # Pass the bookmarked vehicles to the template
     return render(request, 'bookmark.html', {'bookmarks': bookmarks})
-
-@login_required
-def add_bookmark(request, vehicle_id):
-    vehicle = get_object_or_404(Vehicle, id=vehicle_id)
-    bookmark, created = Bookmark.objects.get_or_create(user=request.user, vehicle=vehicle)
-    
-    if created:
-        messages.success(request, "Vehicle bookmarked successfully!")
-    else:
-        messages.info(request, "This vehicle is already bookmarked.")
-
-    return redirect('vehicle_detail', vehicle_id=vehicle.id)
-
-@login_required
-def remove_bookmark(request, vehicle_id):
-    vehicle = get_object_or_404(Vehicle, id=vehicle_id)
-    Bookmark.objects.filter(user=request.user, vehicle=vehicle).delete()
-    messages.success(request, "Bookmark removed successfully.")
-    
-    return redirect('vehicle_detail', vehicle_id=vehicle.id)
 
 @login_required
 def toggle_bookmark(request, vehicle_id):
     vehicle = get_object_or_404(Vehicle, id=vehicle_id)
     bookmark, created = Bookmark.objects.get_or_create(user=request.user, vehicle=vehicle)
-    if not created:  # If bookmark already exists, remove it
-        bookmark.delete()
-    return redirect('vehicle_detail', vehicle_id=vehicle_id)
+    if created:
+        messages.success(request, "Vehicle bookmarked successfully!")
+    else:
+        bookmark.delete()  # Unbookmark if it already exists
+        messages.info(request, "Bookmark removed.")
+    return redirect('sewajual:full_info', pk=vehicle_id)
--- sewajual/models.py
+++ sewajual/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from main.models import Vehicle
+from django.contrib.auth.models import User
 
 class Katalog(models.Model):
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+
+    bookmarked_by = models.ManyToManyField(User, related_name='bookmarked_vehicles', blank=True)
