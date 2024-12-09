c06a57b27e10b2b8738f39c96fe8fb072484a579
freiazulaika <freiazulaika27@gmail.com>
2024-12-09 13:27:23 +0700

    menambahkan untuk get partner

--- sewajual/urls.py
+++ sewajual/urls.py
@@ -11,9 +11,7 @@ urlpatterns = [
     path('vehicles/adm/<uuid:pk>/edit/', edit_vehicle, name='edit_vehicle'),
     path('vehicles/adm/<uuid:pk>/delete/', delete_vehicle, name='delete_vehicle'),
     path('bookmarks/', include('bookmark.urls', namespace='bookmark')),
-    path('xml/', show_xml, name='show_xml'),
     path('vehicle/json/', show_json, name='show_json'),
-    path('xml/<uuid:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
     path('create-flutter/', create_product_flutter, name='create_vehicle_flutter'),
+    path('vehicle/get-stores/', get_stores, name='get-stores'),
 ]
