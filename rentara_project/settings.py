730988f9efc5f9486eb842ead25f52c948a9e130
raisassc <sakila.raisa@gmail.com>
2024-12-14 14:49:53 +0700

    memperbaiki csrf_trusted_origin

--- rentara_project/settings.py
+++ rentara_project/settings.py
@@ -145,7 +145,7 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://http://raisa-sakila-rentaraproject.pbp.cs.ui.ac.id/", "https://http://raisa-sakila-rentaraproject.pbp.cs.ui.ac.id/", 'http://localhost:65046']
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://raisa-sakila-rentaraproject.pbp.cs.ui.ac.id/", "https://raisa-sakila-rentaraproject.pbp.cs.ui.ac.id/", 'http://localhost:65046']
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:65046',  # Add your Flutter app URL
     'http://127.0.0.1:65046',  # Add your Flutter app URL
