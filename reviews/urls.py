4e4419b072cc5ff7d4069b82695e76f60e4cfc39
meisyanajla <meisyaanajlaa@gmail.com>
2024-12-22 12:04:42 +0700

    Revising URLs on Reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from reviews.views import show_reviews, create_reviews, edit_review, delete_review, create_reviews_ajax
+from authentication import views
+from reviews.views import create_reviews_flutter, delete_reviews_flutter, edit_reviews_flutter, get_vehicle_review_stats, show_reviews, create_reviews, edit_review, delete_review, create_reviews_ajax
 from reviews.views import show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'reviews'
@@ -9,9 +10,15 @@ urlpatterns = [
     path('create-reviews/', create_reviews, name='create_reviews'),
     path('edit-review/<uuid:id>/', edit_review, name='edit_review'),
     path('delete/<uuid:id>/', delete_review, name='delete_review'),
-    path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json'),
-    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('show-reviews/xml/', show_xml, name='show_xml'),
+    path('show-reviews/xml/<uuid:vehicle_id>/', show_json, name='show_json_by_vehicle'),
+    path('show-reviews/json/', show_json, name='show_json'),
+    path('show-reviews/json/<uuid:vehicle_id>/', show_json, name='show_json_by_vehicle'),
+    path('show-reviews/xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('show-reviews/json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('create-reviews-ajax/', create_reviews_ajax, name='create_reviews_ajax'),
+    path('create-reviews-flutter/', create_reviews_flutter, name='create_reviews_flutter'),
+    path('edit-reviews-flutter/', edit_reviews_flutter, name='edit_reviews_flutter'),
+    path('delete-reviews-flutter/', delete_reviews_flutter, name='delete_reviews_flutter'),
+    path('vehicle_review_stats/<int:vehicle_id>/', get_vehicle_review_stats, name='vehicle_review_stats'),
 ]
